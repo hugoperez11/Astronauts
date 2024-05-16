c22d409a63e10d9eebb6a695ba50b438379bfab3
Danil22May <daniilmej88@gmail.com>
2024-05-16 11:56:26 +0200

    adding main.js && loggining logic

---
+++ main.js
@@ -0,0 +1,14 @@
+document.getElementById("login-form").addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const passwordInput = document.getElementById("password");
+  const error = document.getElementById("login-error");
+  const validPassword = "password";
+
+  if (passwordInput.value === validPassword) {
+    window.location.href = "./dashboard.html";
+  } else {
+    error.style.display = "block";
+    passwordInput.value = "";
+  }
+});
